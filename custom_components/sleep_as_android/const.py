1cd906d66c6f3802d50d1110e4e99be759df3a97
IATkachenko <IATkachenko@gmail.com>
2021-02-07 17:19:05 +0300

    feat: create all devices in single integration
    Now we may use one integration for all devices.
    We subscribes at topics in root topic and every subtopic will create own device on the fly.
    Fix #2

--- custom_components/sleep_as_android/const.py
+++ custom_components/sleep_as_android/const.py
@@ -4,6 +4,6 @@ DOMAIN = "sleep_as_android"
 
 schema = {
     vol.Required("name", default="SleepAsAndroid"): str,
-    vol.Required("topic", ): str,
+    vol.Required("root_topic", default="SleepAsAndroid"): str,
     vol.Optional("qos", default=0): int
 }
