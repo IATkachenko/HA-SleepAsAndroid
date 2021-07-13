7de92e882a4093df0510e7ff298dff3ef4981a2e
Igor Tkachenko <IATkachenko@gmail.com>
2021-07-13 13:01:33 +0300

    feat: added device_macro
    This constant contain macro for getting position from topic

--- custom_components/sleep_as_android/const.py
+++ custom_components/sleep_as_android/const.py
@@ -1,9 +1,10 @@
 import voluptuous as vol
 
 DOMAIN = "sleep_as_android"
+DEVICE_MACRO: str = "%%%device%%%"
 
 schema = {
     vol.Required("name", default="SleepAsAndroid"): str,
-    vol.Required("root_topic", default="SleepAsAndroid"): str,
+    vol.Required("topic", default="SleepAsAndroid/%s" % DEVICE_MACRO): str,
     vol.Optional("qos", default=0): int
 }
