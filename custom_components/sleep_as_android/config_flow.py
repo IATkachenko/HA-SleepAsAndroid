26be4cea96aeac078a8ccbcdcdaf203b5d708a78
IATkachenko <IATkachenko@gmail.com>
2022-02-18 18:55:44 +0300

    fix: preload current options in settings dialog

--- custom_components/sleep_as_android/config_flow.py
+++ custom_components/sleep_as_android/config_flow.py
@@ -1,43 +1,74 @@
+from __future__ import annotations
+
 import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.core import callback
 
-from .const import DOMAIN, schema
+from .const import DOMAIN, DEFAULT_NAME, DEFAULT_TOPIC_TEMPLATE, DEFAULT_QOS
 
 
-class SleepAsAndroidFlow(config_entries.ConfigFlow, domain=DOMAIN):
+def get_value(config_entry: config_entries | None, param: str, default=None):
+    """Get current value for configuration parameter.
 
+    :param config_entry: config_entries|None: config entry from Flow
+    :param param: str: parameter name for getting value
+    :param default: default value for parameter, defaults to None
+    :returns: parameter value, or default value or None
+    """
+    if config_entry is not None:
+        return config_entry.options.get(param, config_entry.data.get(param, default))
+    else:
+        return default
+
+
+def create_schema(config_entry: config_entries | None, step: str = "user") -> vol.Schema:
+    """Generates configuration schema.
+
+    :param config_entry: config_entries|None: config entry from Flow
+    :param step: stem name
+    """
+    schema = vol.Schema({})
+    if step == "user":
+        schema = schema.extend({
+            vol.Required("name", default=get_value(config_entry=config_entry, param="name", default=DEFAULT_NAME)): str,
+        })
+
+    schema = schema.extend({
+        vol.Required("topic_template", default=get_value(config_entry=config_entry, param="topic_template", default=DEFAULT_TOPIC_TEMPLATE)): str,
+        vol.Optional("qos", default=get_value(config_entry=config_entry, param="qos", default=DEFAULT_QOS)): int
+    })
+    return schema
+
+
+class SleepAsAndroidConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     def __init__(self):
-        self._data = []
-
-    async def async_step_user(self, user_input=None):
-        if user_input is not None:
-            self._data = user_input
-            return self.async_create_entry(title=self._data['name'], data=self._data)
-
-        return self.async_show_form(
-            step_id="user", data_schema=vol.Schema(schema)
-        )
-
-
-@config_entries.HANDLERS.register(DOMAIN)
-class SleepAsAndroidConfigFlow(SleepAsAndroidFlow, config_entries.ConfigFlow, domain=DOMAIN):
-    def __init__(self):
-        super().__init__()
+        self._config_entry: config_entries | None = None
 
     @staticmethod
     @callback
     def async_get_options_flow(config_entry):
-        return SleepAsAndroidOptionsFlowHandler(config_entry)
+        """Get the options flow for this handler."""
+        return SleepAsAndroidOptionsFlow(config_entry)
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title=user_input["name"], data=user_input)
+
+        return self.async_show_form(
+            step_id="user", data_schema=create_schema(None, step="user")
+        )
 
 
-class SleepAsAndroidOptionsFlowHandler(SleepAsAndroidFlow, config_entries.OptionsFlow):
+class SleepAsAndroidOptionsFlow(config_entries.OptionsFlow):
     def __init__(self, config_entry):
         super().__init__()
         self._config_entry = config_entry
         self._entry_id = config_entry.entry_id
 
-    async def async_step_init(self, input=None):
-        return await self.async_step_user(input)
+    async def async_step_init(self, user_input=None):
+        errors = {}
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
 
+        return self.async_show_form(step_id="init", data_schema=create_schema(self._config_entry, step="init"), errors=errors)
--- custom_components/sleep_as_android/const.py
+++ custom_components/sleep_as_android/const.py
@@ -3,8 +3,6 @@ import voluptuous as vol
 DOMAIN = "sleep_as_android"
 DEVICE_MACRO: str = "%%%device%%%"
 
-schema = {
-    vol.Required("name", default="SleepAsAndroid"): str,
-    vol.Required("topic_template", default="SleepAsAndroid/%s" % DEVICE_MACRO): str,
-    vol.Optional("qos", default=0): int
-}
+DEFAULT_NAME = "SleepAsAndroid"
+DEFAULT_TOPIC_TEMPLATE = "SleepAsAndroid/%s" % DEVICE_MACRO
+DEFAULT_QOS = 0
